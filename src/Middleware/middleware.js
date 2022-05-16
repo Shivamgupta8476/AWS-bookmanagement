39a84fe07c8c240c3a3acfdb4d2c88e015742221
shivam <shivamgupta8476@gmail.com>
2022-05-16 10:36:13 +0530

    final

--- src/Middleware/middleware.js
+++ src/Middleware/middleware.js
@@ -10,11 +10,12 @@ const authentication = async function (req, res, next) {
     let token = req.headers["x-api-key"] || req.headers["x-Api-Key"];
 
     if (!token) {
-      return res.status(401).send({ status: false, msg: "Token must be Present" });
+      return res.status(403).send({ status: false, msg: "Token must be Present" });
     }
 
     let decodedtoken = jwt.verify(token, "group51"); // to verify that signature is valid or not
-
+    /* console.log(decodedtoken) */
+    if(!decodedtoken) return res.status(403).send({status:false,msg:"Incorrect token"})
     next();
   } catch (err) {
     res.status(500).send({ status: false, error: err.message });
@@ -40,15 +41,16 @@ const authorization = async function (req, res, next) {
       }
     }
 
-    const findbookdatabyId = await BookModel.findById(id).select({userId:1})
-    if(!findbookdatabyId){
+    const findbookdatabyId = await BookModel.findById(id).select({ userId: 1 })
+    //console.log(findbookdatabyId)
+    if (!findbookdatabyId) {
       return res.status(400).send({ status: false, msg: "Incorrect BookId" });
     }
 
     let decodedtoken = jwt.verify(token, "group51");
-
+    if(!decodedtoken) return res.status(403).send({status:false,msg:"Incorrect token"})
     if (decodedtoken.UserId != findbookdatabyId.userId)
-      return res.status(403).send({ status: false, msg: "Sorry,You cannot access" });
+      return res.status(401).send({ status: false, msg: "Sorry,You cannot access" });
 
     next(); //if match then move the execution to next
   } catch (err) {
